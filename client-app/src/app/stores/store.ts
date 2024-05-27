c442855a219b744b9a074a40ab6d45890f583b3a
ashrithapabba <ashrithapabba97@gmail.com>
2024-05-26 18:43:04 -0700

    s19

--- client-app/src/app/stores/store.ts
+++ client-app/src/app/stores/store.ts
@@ -3,14 +3,16 @@ import ActivityStore from "./activityStore";
 import CommonStore from "./commonStore";
 import UserStore from "./userStore";
 import ModalStore from "./modalStore";
-import ProfileStore from "./ProfileStore";
+import CommentStore from "./commentStore";
+import ProfileStore from "./profileStore";
 
 interface Store {
     activityStore: ActivityStore
     commonStore: CommonStore;
     userStore: UserStore;
     modalStore: ModalStore;
-    profileStore: ProfileStore
+    profileStore: ProfileStore;
+    commentStore: CommentStore;
 }
 
 export const store: Store ={
@@ -18,7 +20,8 @@ export const store: Store ={
     commonStore: new CommonStore(),
     userStore: new UserStore(),
     modalStore: new ModalStore(),
-    profileStore: new ProfileStore()
+    profileStore: new ProfileStore(),
+    commentStore: new CommentStore()
 }
 
 export const StoreContext = createContext(store);
